260d4c63d1112a7e7d345f503bc192f8acd2001d
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 17:48:55 +0530

    Added: code for Employee working 100 hrs

--- src/employeebuilder/EmployeeWage.java
+++ src/employeebuilder/EmployeeWage.java
@@ -6,30 +6,32 @@ public class EmployeeWage
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		System.out.println("###..Welcome to Employee Wage Computation..###");
 		
-		int empHrs=0, empWage = 0, totalEmpWage = 0;
-		
-		for (int day =0; day < NUM_OF_WORKING_DAYS; day++ )
+		int empHrs=0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
-		int empCheck = (int) Math.floor(Math.random() * 10  % 3);
-	      switch (empCheck)
-	      {
-	      	case IS_PART_TIME: 
-	      		empHrs = 4; break;
-	      	case IS_FULL_TIME:
-	      		empHrs = 8; break;
-	      	default:
-	      		empHrs = 0; break;
-	      }
-	      empWage = empHrs * EMP_RATE_PER_HOUR;
-	      totalEmpWage += empWage;
-	      System.out.println("Emp Wage: " + empWage);
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10  % 3);
+			switch (empCheck)
+			{
+	      		case IS_PART_TIME: 
+	      			empHrs = 4; break;
+	      		case IS_FULL_TIME:
+	      			empHrs = 8; break;
+	      		default:
+	      			empHrs = 0; break;
+			}
+			totalEmpHrs += empHrs;
+	      
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 	    }
-		  System.out.println("\nTotal Emp Wage: " + totalEmpWage);		        
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;	
+		System.out.println("Total Emp Wage: " + totalEmpWage);	        
 	 }
 }
