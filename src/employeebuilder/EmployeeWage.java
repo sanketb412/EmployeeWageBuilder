e8cab2961408f3bc630a6847a52fba51bbb4db36
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 17:14:35 +0530

    Added: code for Part time hour and wage

--- src/employeebuilder/EmployeeWage.java
+++ src/employeebuilder/EmployeeWage.java
@@ -8,25 +8,23 @@ public class EmployeeWage
 		// TODO Auto-generated method stub
 		System.out.println("###..Welcome to Employee Wage Computation..###");
 		
-		int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-	    if (empCheck == IS_FULL_TIME)
+		int IS_FULL_DAY = 1;
+		int IS_HALF_DAY = 2;
+		int emphrf = 8;
+		int emphra = 4;
+		int empratehr = 20;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+	    if (empCheck == IS_FULL_DAY)
 	    {
-	         System.out.println("\nEmployee is Present Full day");
+	        System.out.println("\nEmployee is Present Full day \nEmployee Daily Wage in Rupees is:" +emphrf*empratehr);
+	    }
+	    else if (empCheck == IS_HALF_DAY)
+	    {
+	    	System.out.println("\nEmployee is Present Half Day \nEmployee Daily Wage in Rupees is:" +emphra*empratehr);
 	    }
 	    else
 	    {
-	         System.out.println("\nEmployee is Absent");
+	         System.out.println("\nEmployee is Absent \nEmployee Daily Wage is 0 Rupees");
 	    }
-	    int emphr = 8;
-	      int empratehr = 20;
-	      int a;
-	      if (empCheck == 1)
-	      	{
-	    	  a = emphr * empratehr;	    	  
-	    	  System.out.println("The Daily Wages of a empolyee in Rupees is: " +a);
-	      	}  
-
 	}
-
 }
