656a8aeb7de82905098f0f45dc637eab02d30d43
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 17:38:12 +0530

    Added code for Monthly Wage

--- src/employeebuilder/EmployeeWage.java
+++ src/employeebuilder/EmployeeWage.java
@@ -2,23 +2,34 @@ package employeebuilder;
 
 public class EmployeeWage
 {
-
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		System.out.println("###..Welcome to Employee Wage Computation..###");
-
-		int emphrf = 8;
-		int emphra = 4;
-		int empratehr = 20;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
-		int IntValue = (int) Math.round(empCheck);
-	      switch (IntValue)
+		int empHrs=0, empWage = 0, totalEmpWage = 0;
+		
+		for (int day =0; day < NUM_OF_WORKING_DAYS; day++ )
+		{
+		int empCheck = (int) Math.floor(Math.random() * 10  % 3);
+	      switch (empCheck)
 	      {
-	      	case 1: System.out.println("\nEmployee is Present Full Day \nEmployee Daily Wage in Rupees is:" +emphrf*empratehr); break;
-	      	case 2: System.out.println("\nEmployee is Present Half Day \nEmployee Daily Wage in Rupees is:" +emphra*empratehr); break;
-	      	case 0: System.out.println("\nEmployee is Absent \nEmployee Daily Wage is 0 Rupees"); break;
-	      }	         
+	      	case IS_PART_TIME: 
+	      		empHrs = 4; break;
+	      	case IS_FULL_TIME:
+	      		empHrs = 8; break;
+	      	default:
+	      		empHrs = 0; break;
+	      }
+	      empWage = empHrs * EMP_RATE_PER_HOUR;
+	      totalEmpWage += empWage;
+	      System.out.println("Emp Wage: " + empWage);
+	    }
+		  System.out.println("\nTotal Emp Wage: " + totalEmpWage);		        
 	 }
 }
