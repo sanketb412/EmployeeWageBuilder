773eb9a3f58f92c40382594908dd463d0f68860a
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 17:22:21 +0530

    Added: code of EmployeeWage using Switch case

--- src/employeebuilder/EmployeeWage.java
+++ src/employeebuilder/EmployeeWage.java
@@ -7,24 +7,18 @@ public class EmployeeWage
 	{
 		// TODO Auto-generated method stub
 		System.out.println("###..Welcome to Employee Wage Computation..###");
-		
-		int IS_FULL_DAY = 1;
-		int IS_HALF_DAY = 2;
+
 		int emphrf = 8;
 		int emphra = 4;
 		int empratehr = 20;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-	    if (empCheck == IS_FULL_DAY)
-	    {
-	        System.out.println("\nEmployee is Present Full day \nEmployee Daily Wage in Rupees is:" +emphrf*empratehr);
-	    }
-	    else if (empCheck == IS_HALF_DAY)
-	    {
-	    	System.out.println("\nEmployee is Present Half Day \nEmployee Daily Wage in Rupees is:" +emphra*empratehr);
-	    }
-	    else
-	    {
-	         System.out.println("\nEmployee is Absent \nEmployee Daily Wage is 0 Rupees");
-	    }
-	}
+		
+		int IntValue = (int) Math.round(empCheck);
+	      switch (IntValue)
+	      {
+	      	case 1: System.out.println("\nEmployee is Present Full Day \nEmployee Daily Wage in Rupees is:" +emphrf*empratehr); break;
+	      	case 2: System.out.println("\nEmployee is Present Half Day \nEmployee Daily Wage in Rupees is:" +emphra*empratehr); break;
+	      	case 0: System.out.println("\nEmployee is Absent \nEmployee Daily Wage is 0 Rupees"); break;
+	      }	         
+	 }
 }
