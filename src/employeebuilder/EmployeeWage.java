8653fb1d8517e768ca24101edc301dbf49190356
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 17:03:13 +0530

    Added: code for Employee Daily Wage

--- src/employeebuilder/EmployeeWage.java
+++ src/employeebuilder/EmployeeWage.java
@@ -9,15 +9,23 @@ public class EmployeeWage
 		System.out.println("###..Welcome to Employee Wage Computation..###");
 		
 		int IS_FULL_TIME = 1;
-	    double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 	    if (empCheck == IS_FULL_TIME)
 	    {
-	         System.out.println("\nEmployee is Present");
+	         System.out.println("\nEmployee is Present Full day");
 	    }
 	    else
 	    {
 	         System.out.println("\nEmployee is Absent");
 	    }
+	    int emphr = 8;
+	      int empratehr = 20;
+	      int a;
+	      if (empCheck == 1)
+	      	{
+	    	  a = emphr * empratehr;	    	  
+	    	  System.out.println("The Daily Wages of a empolyee in Rupees is: " +a);
+	      	}  
 
 	}
 
