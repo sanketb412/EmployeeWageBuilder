bfbf27d0759ca48871c6fd70bd1090124a09d186
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 16:37:37 +0530

    Added: Code of Welcoming

---
+++ src/employeebuilder/EmployeeWage.java
@@ -0,0 +1,13 @@
+package employeebuilder;
+
+public class EmployeeWage
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation");
+
+	}
+
+}
