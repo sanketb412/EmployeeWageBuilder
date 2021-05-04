192adbc4324f943bc982b9416bbf5e8cf0a200ec
sanketb412 <sanketbagde38@gmail.com>
2021-05-04 18:53:17 +0530

    Added: code of Multiples Companies using Object Class Method

--- src/employeebuilder/EmployeeWage.java
+++ src/employeebuilder/EmployeeWage.java
@@ -1,37 +1,37 @@
 package employeebuilder;
 
-public class EmployeeWage
-{
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
-	
-	public static int EmpWage()
-	{
-		int empHrs=0, totalEmpHrs = 0, totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-		{
+public class EmployeeWage {
+	public static final int PART_TIME = 1;
+	public static final int FULL_TIME = 2;
+
+	public static int EmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10  % 3);
-			switch (empCheck)
-			{
-	      		case IS_PART_TIME: 
-	      			empHrs = 4; break;
-	      		case IS_FULL_TIME:
-	      			empHrs = 8; break;
-	      		default:
-	      			empHrs = 0; break;
+			int empCheck = (int) (Math.random() * 10 % 3);
+			switch (empCheck) {
+			case PART_TIME:
+				empHrs = 4;
+				break;
+			case FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+				break;
 			}
 			totalEmpHrs += empHrs;
-	    }
-		totalWorkingDays = totalEmpHrs * EMP_RATE_PER_HOUR;
-		return totalWorkingDays;
-	 }
-	public static void main(String[] args)
-	{	
+			System.out.println("Day#: " + totalWorkingDays + "  Emp Hr: " + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("\nTotal Emp Wage for Company " + company + " is: " + totalEmpWage + "\n");
+		System.out.println("------------------------------------------------");
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args) {
 		System.out.println("\n..####...Welcome to Employee Wage Computation...####..");
-		System.out.println("\n\t\tTotal Emp Salary: " +EmpWage());
+		EmpWage("DMart", 20, 20, 100);
+		EmpWage("Reliance", 30, 15, 150);
 	}
 }
